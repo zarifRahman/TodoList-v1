2908375c29712397432bf4cc49d73229f5274f82
zarifRahman <zarif.shahriar.rahman@gmail.com>
2019-01-20 20:25:34 +0600

    finish  work and task page

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("public"));
 
 let inputValueArray = ["Buy Food","Cook food"];
+let workArray = ["learn JS","Complete Project"];
+let taskArray = ["EJS","API"];
 
 app.get("/",function(req,res){
   var options = { weekday: 'long',
@@ -22,12 +24,46 @@ app.get("/",function(req,res){
   res.render('list',{title : date, newValue : inputValueArray});
 })
 
-app.post("/",function(req,res){
-  let inputValue = req.body.newItem;
-  inputValueArray.push(inputValue);
-  res.redirect("/");
+app.get("/work",function(req,res){
+  res.render('list',{title : "Work",newValue : workArray});
 })
 
+app.get("/task",function(req,res){
+  res.render("list",{title : "Completed-Task", newValue : taskArray})
+})
+
+// app.post("/",function(req,res){
+//   console.log(req.body);
+//   if(req.body.button === 'Work'){
+//     let workList = req.body.newItem;
+//     workArray.push(workList);
+//     res.redirect("/work");
+//   }else{
+//     let inputValue = req.body.newItem;
+//     inputValueArray.push(inputValue);
+//     res.redirect("/");
+//   }
+// })
+app.post("/",function(req,res){
+  console.log(req.body);
+  switch (req.body.button) {
+  case 'Work':
+    let workList = req.body.newItem;
+    workArray.push(workList);
+    res.redirect("/work");
+    break;
+  case "Completed-Task":
+    let taskList = req.body.newItem;
+    taskArray.push(taskList);
+    res.redirect("/task");
+    break;
+  default:
+    let inputValue = req.body.newItem;
+    inputValueArray.push(inputValue);
+    res.redirect("/");
+
+  }
+})
 
 app.listen(3000,function(req,res){
   console.log("Server running at 3000")
